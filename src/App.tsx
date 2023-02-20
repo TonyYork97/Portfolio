0eeea17792e89fd47e288787ee0b673e465adfe0
TonyYork97 <ayu2600@mail.ru>
2023-02-21 01:35:22 +0400

    add words

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ import { Home } from './pages/Home'
 import { Header } from './components/Header'
 import { ButtonScroll } from './components/ButtonScroll'
 import './styles/App.scss'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
 
+  useEffect(() => {
+    setIsLoading(false)
+  }, [])
+
+  if (isLoading) {
+    return <><div>Loading...</div></>
+  }
   return (
     <div className="App">
       <Header />
--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-// import ContactA from '../assets/contact-text/a.png'
-// import ContactTo from '../assets/contact-text/to.png'
-// import ContactNo from '../assets/contact-text/no.png'
-// import ContactLose from '../assets/contact-text/lose.png'
-// import ContactMe from '../assets/contact-text/me.png'
+import ContactA from '../assets/a.png'
+import ContactTo from '../assets/to.png'
+import ContactNo from '../assets/no2.png'
+import ContactLose from '../assets/lose.png'
+import ContactMe from '../assets/me2.png'
 import Telephone from '../assets/telephone.svg'
 import Whatsapp from '../assets/whatsapp2.svg'
 import Viber from '../assets/viber2.svg'
@@ -19,11 +19,11 @@ export const ContactSection = () => {
   return (
     <section id='section-contacts' className="contacts">
       <div className='crooked-contact'>
-        {/* <MotionViewImage src={ContactA} cn='crooked-a' delay={0.1} />
+        <MotionViewImage src={ContactA} cn='crooked-a' delay={0.1} />
         <MotionViewImage src={ContactTo} cn='crooked-to' delay={0.2} />
         <MotionViewImage src={ContactNo} cn='crooked-no' delay={0.3} />
         <MotionViewImage src={ContactLose} cn='crooked-lose' delay={0.4} />
-        <MotionViewImage src={ContactMe} cn='crooked-me' delay={0.5} /> */}
+        <MotionViewImage src={ContactMe} cn='crooked-me' delay={0.5} />
       </div>
       <div className='spherical-block'>
         {/* <SphericalBlock /> */}
